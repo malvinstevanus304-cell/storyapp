1dce416355b1c1929cc1ee9058ea45baf9755839
malvinstevanus304 <malvinstevanus304@gmail.com>
2025-10-03 00:22:50 +0700

    update

--- webpack.common.js
+++ webpack.common.js
@@ -5,32 +5,40 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
-    sw: path.resolve(__dirname, 'src/public/sw.js'), // ← entry point untuk SW
+    sw: path.resolve(__dirname, 'src/public/sw.js'), // entry untuk service worker
   },
   output: {
-    filename: '[name].bundle.js', // menghasilkan app.bundle.js dan sw.bundle.js
+    filename: '[name].bundle.js', // hasil: app.bundle.js dan sw.bundle.js
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // biar setiap build dist bersih dulu
   },
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]', // rapihin gambar di dist/images
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
-      excludeChunks: ['sw'], // jangan masukkan sw.bundle.js ke HTML
+      excludeChunks: ['sw'], // jangan inject sw.bundle.js ke index.html
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          from: path.resolve(__dirname, 'src/public'),
+          to: path.resolve(__dirname, 'dist'),
           globOptions: {
-            ignore: ['sw.js'], // sw.js sudah menjadi entry point, jangan dicopy
+            ignore: ['**/sw.js'], // sw.js jangan dicopy lagi, sudah jadi entry
           },
         },
       ],
